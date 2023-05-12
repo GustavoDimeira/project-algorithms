3e87e0691152e9a30fdc2b501255b993c9a94326
GustavoDimeira <gustavodimeira1@gmail.com>
2023-05-12 14:53:28 -0400

    feat: counte time ocurrency

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    try:
+        result = 0
+        if (target_time == None): raise TypeError
+        for period in permanence_period:
+            if (target_time in range(period[0], period[1] + 1)):
+                result += 1
+        return result
+    except TypeError:
+        return None
+
+# print(study_schedule([(1, 2), (1, 3), (2, 3)], None))
