19716c9d2fe9bc64595782e280a80bda8566c4aa
GustavoDimeira <gustavodimeira1@gmail.com>
2023-05-12 15:03:09 -0400

    fix

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,7 +1,8 @@
 def study_schedule(permanence_period, target_time):
     try:
         result = 0
-        if (target_time == None): raise TypeError
+        if (target_time is None):
+            raise TypeError
         for period in permanence_period:
             if (target_time in range(period[0], period[1] + 1)):
                 result += 1
