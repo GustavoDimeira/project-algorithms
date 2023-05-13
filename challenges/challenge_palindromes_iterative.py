1f827520d01589db21ec73d243c026dfe7f05f65
GustavoDimeira <gustavodimeira1@gmail.com>
2023-05-13 15:14:58 -0400

    fix: decreasing func complexid

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,10 +1,8 @@
-def recursive_inversor(word, low_index, high_index):
-    if (low_index == high_index):
-        return word[low_index]
-    return recursive_inversor(word, low_index+1, high_index) + word[low_index]
-
-
 def is_palindrome_iterative(word):
     if (not word or len(word) == 0):
         return False
-    return (recursive_inversor(word, 0, len(word) - 1) == word)
+    reverse_word = ""
+    word_size = len(word)
+    for i in range(1, word_size + 1):
+        reverse_word += word[word_size - i]
+    return reverse_word == word
