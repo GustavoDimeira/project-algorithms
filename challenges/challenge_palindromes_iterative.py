fe83ed10fca19fb14ed107481febbedea9c4c441
GustavoDimeira <gustavodimeira1@gmail.com>
2023-05-13 09:47:28 -0400

    x

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,10 @@
+def recursive_inversor(word, low_index, high_index):
+    if (low_index == high_index):
+        return word[low_index]
+    return recursive_inversor(word, low_index+1, high_index) + word[low_index]
+
+
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if (not word or len(word) == 0):
+        return False
+    return (recursive_inversor(word, 0, len(word) - 1) == word)
